5fae91a8f9c31853ff7b90f59ad14e446913af2e
Taylor Rogers <taylor.rogers71@gmail.com>
2022-09-29 08:06:20 -0700

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  rules: {},
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+}
