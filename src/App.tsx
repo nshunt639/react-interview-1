e53c0d6656221235a60d7c961c151aa5e7212448
Taylor Rogers <taylor.rogers71@gmail.com>
2022-10-04 16:25:40 -0700

    Average test

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,27 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className='App-link'
-          href='https://lightcast.io/'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Checkout Lightcast.io!
-        </a>
+        <p>
+          {`Please edit this app such that the following input accepts any integer and displays the average of all inputs.
+          Pressing the 'Clear' button should clear the running average (output should be 'N/A').`}
+          <br />
+          E.g.
+          <br />
+          Input: 5; Output: 5
+          <br />
+          Input: 10; Output: 7.5
+          <br />
+          Input: 15; Output: 10
+          <br />
+          Input: 20; Output: 12.5`
+        </p>
+        <input placeholder='Give me a number!' />
+        <div>
+          {' '}
+          <span style={{ fontStyle: 'bold' }}>Average: </span>OUTPUT HERE
+        </div>
+        <button>Get Average</button>
+        <button>Clear</button>
       </header>
     </div>
   )
