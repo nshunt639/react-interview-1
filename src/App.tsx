cec13506a90fd25e2ec8d51c9612b6445546fa7a
Taylor Rogers <taylor.rogers71@gmail.com>
2022-10-05 11:27:09 -0700

    add state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,39 @@
-import React from 'react'
-import logo from './lightcast_logo.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+
+type Color = 'red' | 'blue';
+type State = {
+  color: Color;
+  count: number;
+};
 
 function App() {
+  const [state, setState] = useState<State>({ count: 0, color: 'red' });
+
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          {`Please edit this app such that the following input accepts any integer and displays the average of all inputs.
-          Pressing the 'Clear' button should clear the running average (output should be 'N/A').`}
-          <br />
-          E.g.
-          <br />
-          Input: 5; Output: 5
-          <br />
-          Input: 10; Output: 7.5
-          <br />
-          Input: 15; Output: 10
-          <br />
-          Input: 20; Output: 12.5`
-        </p>
-        <input placeholder='Give me a number!' />
-        <div>
-          {' '}
-          <span style={{ fontStyle: 'bold' }}>Average: </span>OUTPUT HERE
-        </div>
-        <button>Get Average</button>
-        <button>Clear</button>
-      </header>
+      <ButtonWrapper />
+      <Display count={state.count} color={state.color} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
+const ButtonWrapper = () => {
+  return (
+    <>
+      <button>Increment</button>
+      <button>Swap Color</button>
+    </>
+  );
+};
+
+const Display = ({ count, color }: { count: number; color: string }) => {
+  return (
+    <>
+      <div>Current Count: {count}</div>
+      <div>Current Color: {color}</div>
+    </>
+  );
+};
